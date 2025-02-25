0a02c6ec3a0dbd38b7beaa4daa9b87ead2eb0b70
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-02-25 14:52:25 -0500

    MainComponent.js updated with deeper useState function

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,10 +1,14 @@
+import { useState} from "react";
 import styles from "../styles/Home.module.css";
 
 export default function MainComponent() {
+    const [WelcomeMessage, setWelcomeMessage] = useState(true);
+
     return (
-    <div className={styles.container}>
+    <div className={styles.mainContainer}>
         <h1>Welcome to the Midterm Practical App</h1>
-        <p>Select an item from the list or add a new one.</p>
+        {WelcomeMessage && <p>Select and item from the list or add a new one.</p>}
+        <button onClick ={() => setWelcomeMessage(false)}>Hide Message</button>
         </div>
         );
 }
