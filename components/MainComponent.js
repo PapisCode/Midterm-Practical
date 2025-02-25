6b375f690bb8027fc5097713986e7ebbb9675c8a
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-02-25 14:13:57 -0500

    Added MainComponent.js to components folder

---
+++ components/MainComponent.js
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css";
+
+export default function MainComponent() {
+    return (
+    <div className={styles.container}>
+        <h1>Welcome to the Midterm Practical App</h1>
+        <p>Select an item from the list or add a new one.</p>
+        </div>
+        );
+}
