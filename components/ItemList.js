ff7804325b71209dc853007f0b2c9e63784cfce0
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-02-25 13:10:07 -0500

    Components folder created and added ItemList.js

---
+++ components/ItemList.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import styles from "../styles.ItemList.module.css";
+
+export default function ItemList({ items, onSelect, addItem }) {
+    const [newItem, setNewItem] = useState(" ");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (newItem.trim() === "") return;
+        addItem(mewItem);
+        setNewItem("");
+};
+
+return (
+    <div className={styles.listContainer}>
+        <h2>Item List</h2>
+        <ul>
+            {items.length > 0 ? (
+                items.map((item, index) => (
+                    <li key={index} onClick={() => onSelect(item)}>
+                        {item}
+                        </li>
+                ))
+                ) : (
+                <p>No items found.</p>
+            )}
+        </ul>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={newItem}
+                onChange={(e) => setNewItem(e.target.value)}
+                placeholder="Add an item"
+            />
+            <button type="submit">Add</button>
+        </form>
+        </div>
+);
+}
