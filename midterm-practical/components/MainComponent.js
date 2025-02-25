5202d319154e57e2ca1907cd66136f68b715e462
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-02-25 15:49:40 -0500

    Reorganized components folder and restored missing files

---
+++ midterm-practical/components/MainComponent.js
@@ -0,0 +1,14 @@
+import { useState} from "react";
+import styles from "../styles/Home.module.css";
+
+export default function MainComponent() {
+    const [WelcomeMessage, setWelcomeMessage] = useState(true);
+
+    return (
+    <div className={styles.mainContainer}>
+        <h1>Welcome to the Midterm Practical App</h1>
+        {WelcomeMessage && <p>Select and item from the list or add a new one.</p>}
+        <button onClick ={() => setWelcomeMessage(false)}>Hide Message</button>
+        </div>
+        );
+}
