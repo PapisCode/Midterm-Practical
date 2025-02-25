50676645e0a53f3d5b5689ce2724f7701a0c91e6
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-02-25 16:43:16 -0500

    Added a better selection to items on index.js

--- midterm-practical/pages/index.js
+++ midterm-practical/pages/index.js
@@ -5,12 +5,17 @@ import ItemDetails from "../components/ItemDetails";
 import styles from "../styles/Home.module.css";
 
 export default function HomePage() {
-  const [items, setItems] = useState(["Item 1", "Item 2", "Item 3"]);
+  const [items, setItems] = useState([
+{ name: "Drake", albums: ["Her Loss", "Certified Lover Boy", "Scorpion"] },
+{ name: "Tory Lanez", albums: ["Sorry 4 What", "Alone at Prom", "Daystar"] },
+{ name: "Lil Baby", albums: ["It's Only Me", "My Turn", "Harder Than Ever"] }
+  ]);
+
   const [selectedItem, setSelectedItem] = useState(null);
 
   const addItem = (item) => {
     if (item.trim() === "") return;
-  setItems([...items, item]);
+  setItems([...items, {name: item, albums: []}]);
 };
 
 return (
