955f96bc99eb109aff3e49401ce6652117be8bba
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-02-25 14:48:17 -0500

    Updated ItemDetails.js with a deeper function of useState

--- components/ItemDetails.js
+++ components/ItemDetails.js
@@ -1,6 +1,13 @@
+import { useState, useEffect } from "react";
 import styles from "../styles/ItemDetails.modules.css";
 
 export default function ItemDetails({ item }) {
+    const [selecteditem, setSelectedItem] = useState(item);
+
+    useEffect(() => {
+        setSelectedItem(item);
+    }, [item]);
+    
     return (
         <div className={styles.detailsContainer}>
             <h2>Item Details</h2>
